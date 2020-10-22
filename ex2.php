a953ba0ae0013fe3aaf9a3003037688207bc039f
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-22 11:27:12 +0100

    Update ex2.php

--- ex2.php
+++ ex2.php
@@ -1,11 +1,15 @@
 <?php
-$nota=12;
-if ($nota <10) {
+$nota=-10;
+if ($nota> 0 && $nota <10) {
 	echo "aluno <span style='color:#ff0000'>Reprovado</span> com ".$nota. " valores";
 	echo "<br>Deverá trabalhar mais para alcançar resultados positivos";
 }
 elseif ($nota>=10 && $nota<=20) {
 echo "aluno <span style='color:#00ff00'>Aprovado</span> com ".$nota. " valores";
 	echo "<br>Muito bom trabalho";
+
+}
+else{
+        echo "aluno <span style='color:#00ff00'>invalido</span> com ".$nota. " valores";
 }
 ?>
