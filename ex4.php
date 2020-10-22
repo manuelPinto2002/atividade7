4b97b90c8ee676f0436d78f66107232be2cfc548
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-22 11:02:10 +0100

    Update ex4.php

--- ex4.php
+++ ex4.php
@@ -1,12 +1,20 @@
 <?php
 $numero =50;
-switch ($numero) {
+switch (true) {
 	case ($numero >100):
 		echo "valor muito alto";
 		break;
-	
-	default:
-		# code...
+	case ($numero <80 && $numero >51):
+		echo "valor medio";
+		break;
+	case ($numero ===50):
+		echo "valor perfeito";
+		break;
+	case ($numero <=10):
+		echo "valor muito baixo";
+		break;
+	case ($numero ==0):
+		echo "sem valor";
 		break;
 }
 ?>
